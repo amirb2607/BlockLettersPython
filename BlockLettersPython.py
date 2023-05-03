159ef251681bcdd552443a64e78af3938059c73a
amirb2607 <amirbadrudeen@gmail.com>
2023-05-03 15:44:09 +0000

    Add files via upload

---
+++ BlockLettersPython.py
@@ -0,0 +1,14 @@
+#Name: @TheBotMir
+#Fun Fact: I Am The Best Streamer on Twitch.Tv/TheBotMir
+
+space_1 = ' '
+space_2 = '  '
+space_3 = '   '
+
+print(space_2 + "A" + space_2 + space_3 +"BBBB" + space_1)
+print(space_1+"A"+space_1+"A" + space_1 + space_3 + "B" + space_3 +"B")
+print("A" + space_3 + "A" + space_3 + "B" + space_3 +"B")
+print("AAAAA" + space_3 + "BBBB" + space_1)
+print("A" + space_3 + "A" + space_3 + "B" + space_3 +"B") 
+print("A" + space_3 + "A" + space_3 + "B" + space_3 +"B")
+print("A" + space_3 + "A" + space_3 + "BBBB" + space_1)
